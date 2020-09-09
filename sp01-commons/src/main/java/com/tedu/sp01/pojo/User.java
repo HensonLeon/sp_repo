927a6cafbfe76c72a294935d19bc922f709be90b
lidaheng <lidaheng@HENG.qstc.com>
2020-09-09 16:25:34 +0800

    alertuser

--- sp01-commons/src/main/java/com/tedu/sp01/pojo/User.java
+++ sp01-commons/src/main/java/com/tedu/sp01/pojo/User.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class User {
+public class User{
 	private Integer id;
 	private String username;
 	private String password;
